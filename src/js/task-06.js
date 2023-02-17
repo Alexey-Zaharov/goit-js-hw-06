01c74050eb58414ad96d12309b875157d0bb9319
Alex <112801683+Alexey-Zaharov@users.noreply.github.com>
2023-02-17 10:18:31 +0200

    Update task-06.js

--- src/js/task-06.js
+++ src/js/task-06.js
@@ -4,7 +4,24 @@ inputArea.addEventListener("blur", () => {
   if (
     inputArea.value.length.toString() !== inputArea.getAttribute("data-length")
   ) {
+    inputArea.classList.remove("valid");
     return inputArea.classList.add("invalid");
   }
-  return inputArea.classList.replace("invalid", "valid");
+  inputArea.classList.remove("invalid");
+  return inputArea.classList.add("valid");
 });
+
+// inputArea.addEventListener("blur", () => {
+//   inputArea.value.length.toString() !== inputArea.getAttribute("data-length")
+//     ? inputArea.classList.add("invalid")
+//     : inputArea.classList.replace("invalid", "valid");
+// });
+
+// inputArea.addEventListener("blur", () => {
+//   if (
+//     inputArea.value.length.toString() !== inputArea.getAttribute("data-length")
+//   ) {
+//     return inputArea.classList.add("invalid");
+//   }
+//   return inputArea.classList.replace("invalid", "valid");
+// });
